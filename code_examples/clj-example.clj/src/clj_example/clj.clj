6d935c48cad028a3a5dad721b58ecd20d337745f
Eleonore9 <eleonore.mayola@ymail.com>
2015-04-12 01:10:35 -0400

    Completed the clj example

--- code_examples/clj-example.clj/src/clj_example/clj.clj
+++ code_examples/clj-example.clj/src/clj_example/clj.clj
@@ -3,18 +3,41 @@
   (:gen-class))
 
 
-(def data
+(def data ;; Get data from the csv
   (str/split-lines (slurp "resources/data.csv")))
 
-(def headers
+(def headers ;; Get column headers
   (str/split (first data) #","))
 
 (defn list-data []
+  "Builds a list of maps with the data."
   (mapv #(zipmap headers
                  (str/split % #","))
         (rest data)))
 
+(defn transform-keys
+  "Turns keys into keywords and deals with spaces."
+  [str-key]
+  (-> str-key
+      clojure.string/lower-case
+      (clojure.string/replace #" " "-")
+      keyword))
+
+(defn keywordize-keys-with-space
+  "Recursively transforms all map keys from strings to keywords and deals
+  with the space for 2 words keys."
+  [data-map]
+  (->> data-map
+       (mapv #(hash-map (transform-keys (key %))
+                       (val %)))
+       (reduce merge)))
+
 (defn -main
   "List humanitarian data from 2014."
   []
-  (list-data))
+  (->> (list-data)
+       (mapv #(keywordize-keys-with-space %))
+       (keep #(if (> (read-string (:total-affected %)) 0) %))
+       (mapv #(select-keys % [:country-name :disaster-subgroup]))
+       (map #(format "%s was affected by a %s disaster" (:country-name %)
+                     (:disaster-subgroup %)))))
